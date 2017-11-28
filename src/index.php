13e4e99e675b6e14592865154964f0726cd5e9eb
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 14:50:08 -0500

    functions p1

--- src/index.php
+++ src/index.php
@@ -3,15 +3,17 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
 
+  function replay( Request $request, Response $response ) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("What up doe, $name");
+    return $response;
+  }
+
   require './vendor/autoload.php';
 
   $app = new \Slim\App;
-  $app->get('/hello/{name}', function (Request $request, Response $response) {
-  $name = $request->getAttribute('name');
 
-  $response->getBody()->write("Hello, $name");
-    return $response;
-  });
+  $app->get('/hello/{name}', replay);
 
   $app->run();
 
