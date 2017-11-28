7d94e774496276fde11ae259ae65d6797802d900
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 15:36:57 -0500

    errors

--- src/endpoints/states.php
+++ src/endpoints/states.php
@@ -8,14 +8,14 @@
   $db_name = getenv('DB_NAME');
   $db_user = getenv('DB_USER');
   $db_pass = getenv('DB_PASS');
-  $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+  //$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
   // DATABASE CONNECTION CODE
 
   function states( Request $request, Response $response ) {
 
-    $results = $db->query("Select * from states");
+  //  $results = $db->query("Select * from states");
 
-    $response->getBody()->write( 'now' );
+    $response->getBody()->write( $db_host );
 
     return $response;
   }
