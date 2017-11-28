a6dee43f5b5d78f5d6dc6f5eed4a562f13a57dde
thevictorgreen <thevictorgreen@gmail.com>
2017-11-28 15:45:47 -0500

    db test 1

--- src/endpoints/states.php
+++ src/endpoints/states.php
@@ -3,21 +3,20 @@
   use \Psr\Http\Message\ServerRequestInterface as Request;
   use \Psr\Http\Message\ResponseInterface as Response;
 
-  // DATABASE CONNECTION CODE
-  $db_host = getenv('DB_HOST');
-  $db_name = getenv('DB_NAME');
-  $db_user = getenv('DB_USER');
-  $db_pass = getenv('DB_PASS');
-  //$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
-  // DATABASE CONNECTION CODE
 
   function states( Request $request, Response $response ) {
 
+    // DATABASE CONNECTION CODE
     $db_host = getenv('DB_HOST');
+    $db_name = getenv('DB_NAME');
+    $db_user = getenv('DB_USER');
+    $db_pass = getenv('DB_PASS');
+    $db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',$db_user,$db_pass);
+    // DATABASE CONNECTION CODE
 
-  //  $results = $db->query("Select * from states");
+    $results = $db->query("Select * from states");
 
-    $response->getBody()->write( $db_host );
+    $response->getBody()->write( $results->rowCount() );
 
     return $response;
   }
